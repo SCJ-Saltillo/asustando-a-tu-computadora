d8a60bcf406c984786a74fbe271799603ae24e72
Orlando Alvarez <92808035+OrlolvareZ@users.noreply.github.com>
2022-10-24 23:31:15 -0500

    Added project files

---
+++ README.md
@@ -0,0 +1,26 @@
+Click [here](#en) to go go the translated version of the `readme`
+
+# Asusta a tus amigos usando inteligencia artificial de Google!
+
+Este repositorio hospeda la demostración del uso de la plataforma Google Teachable Machine para crear un modelo de inteligencia artificial, llevada a cabo durante el evento de la Sociedad Científica Juvenil "La Casita del Terror" en su iteración del año 2022.
+
+El proyecto consiste en un pequeño juego en el que el usuario:
+
+1. Pega el enlace del modelo de I.A. que generó a través de Google Teachable Machine. Este modelo permite detectar la cercanía/lejanía con respecto a la cámara del teléfono. De no haber podido generarlo, se puede dejar el campo en blanco y se procederá con un modelo general.
+2. Se toma/ sube una fotografía que provoque miedo.
+3. Tras subir la foto, aparecerá un texto muy pequeño. Se debe pedir a alguien más que intente leerlo.
+4. Una vez que la otra persona se acerque, se reproducirá un jumpscare con la imagen subida y un sonido por defecto.
+
+<!-- Documentation in English, to whomever outside our dissemination group may interest: -->
+<br>
+
+<h1 id="en"> Scare your friends using Google-powered AI! </h1>
+
+This repository hosts the demo for the use of Google Teachable Machine to create an artificial intelligence model, carried out during the Sociedad Científica Juvenil's event “La Casita del Terror” in its 2022 edition.
+
+The project consists of a small game in which the user:
+
+1. Paste the link to the I.A. model you generated via Google Teachable Machine. This model makes it possible to detect the proximity/distance to the camera of the phone. If the user was not able to generate it, the field can be left blank and proceed with a general model.
+2. A photograph is taken/ uploaded that may cause fear.
+3. After uploading the photo, a very small text will appear. Someone else should be asked to try to read it.
+4. Once the other person approaches, a jumpscare will be played with the uploaded image and a default sound.
---
+++ sketch.js
@@ -0,0 +1,211 @@
+// Classifier object and model URL
+let classifier;
+let imageModelURL;
+let triedLoadingModel;
+
+// Classification variables
+let label;
+let confidence;
+
+// Video capture
+let video;
+let flippedVideo;
+
+// Variables for picture selection
+let fileSelector;
+let selectedImg;
+
+// To indicate jumpscare must be played
+let playJumpscare_;
+let loadAnim;
+let jumpSound;
+let jumpSoundPlayed;
+let loading;
+
+function preload() {
+  
+  // Couldn't load ml5 libraries. Force user to reload.
+  if(typeof ml5 === 'undefined'){
+    alert('Estamos teniendo problemas con tu internet. Vamos a recargar la página.');
+    location.reload();
+  }
+  
+  imageModelURL = prompt('Pega aquí el enlace a tu modelo');
+
+  if(imageModelURL)
+    imageModelURL += "model.json";
+  else{
+    imageModelURL = "https://teachablemachine.withgoogle.com/models/c8dPzDi1v/model.json";
+    alert('Se usará un modelo por defecto, pero quizás no funcione tan bien.');
+  }
+
+  classifier = ml5.imageClassifier(imageModelURL);
+
+  // Notify user if classifier is not yet ready, so they wait to play the prank
+  if(!classifier){
+    alert('Tu modelo está tardando en cargar. Por favor espera un poco.');
+    triedLoadingModel = true;
+  }
+  else
+    triedLoadingModel = false;
+  
+  loadAnim = loadImage('asustando-a-tu-computadora/media/loading.gif');
+  jumpSound = loadSound('asustando-a-tu-computadora/media/jumpSound.mp3');
+  
+}
+
+function setup() {
+  
+  createCanvas(displayWidth, displayHeight);
+  
+  fileSelector = document.getElementById('file-selector');
+  
+  // Gets image file
+  fileSelector.addEventListener('change', (event) => {
+    
+    // Remove other elements
+    document.getElementById("container").innerHTML = "";    
+    
+    loading = true;
+    
+    // Read file as image
+    let image = event.target.files[0];
+    readImage(image);
+    
+  });
+  
+  playJumpscare_ = false;
+  jumpSoundPlayed = false;
+  loading = false;
+}
+
+function draw() {
+  
+  // Indicate that jumpscare must be played once when subject gets close
+    
+  if(label){
+    
+    if(label == 'cerca' || label == 'Cerca')
+      playJumpscare_ = true;
+      
+  }
+  
+  if(playJumpscare_){
+    playJumpscare();
+  }
+  else if (selectedImg){
+    displayDummyText();
+  }
+  else if(loading){
+    image(loadAnim, displayWidth/2, 50);          
+  }
+  
+}
+
+function playJumpscare(){
+  
+  image(selectedImg, 0, 0, displayWidth, displayHeight);
+  
+  if(!jumpSoundPlayed){
+    jumpSound.play();
+    jumpSoundPlayed = true;
+  }
+  
+}
+
+function displayDummyText(){
+  
+  background(245,187,12);
+  fill('black');
+  
+  let middle = displayWidth/2;
+  
+  text("La Casa Decorada: una escalofriante leyenda de Halloween", middle, 10);
+  text("Nuestras primeras protagonistas fueron dos amigas de instituto que", middle, 13);
+  text("acabaron convertidas en rivales en la peor de las noches, la de", middle, 16);
+  text("Halloween. Ambas habían cuidado con mimo el más mínimo detalle de", middle, 19);
+  text("sus disfraces de brujas y ambas también tenían preparado un auténtico", middle, 22);
+  text("espectáculo terrorífico en sus casas para impresionar al jurado que", middle, 25);
+  text("decidiría quién sería la familia más espeluznante.", middle, 28);
+  text("La cosa estaba muy reñida, por lo que Charlotte acudió a su belleza y encantos", middle, 31);
+  text("naturales para conseguir el voto masculino y ganar el primer premio al mejor", middle, 34);
+  text("disfraz. Katie, como sabía de las artimañas de su amiga, se lo jugó todo a una carta:", middle, 37);
+  text("su casa debía ser las más aterradora. Proyecciones de asesinos en serie sobre las", middle, 40);
+  text("ventanas, 50 gatos negros maullando en su jardín, humo, esqueletos danzantes,", middle, 43);
+  text("pirotécnica… Miraras donde miraras el miedo se dibujaba en la cara del jurado. Y", middle, 46);
+  text("cuando parecía que nada más podía pasar, llegó el truco final.", middle, 49);
+  text("Katie había preparado su propio ahorcamiento falso en el árbol de su jardín", middle, 52);
+  text("para despedir al jurado. Aplausos y gritos de sorpresa envolvieron el momento. Ya", middle, 55);
+  text("había ganadora. Katie fue jaleada como la triunfadora. Pero nadie pudo recoger el", middle, 58);
+  text("premio. Algo había fallado con el truco de cuerdas y sistemas de seguridad y las", middle, 61);
+  text("convulsiones de su cuerpo no habían sido teatro. Katie estaba muerta. Este mito,", middle, 64);
+  text("conocido como La Casa Decorada, nos recuerda que no todo vale para ganar y", middle, 67);
+  text("que poner unos límites nos puede salvar la vida.", middle, 70);
+  
+}
+
+// Get a prediction for the image received by parameter
+function classifyVideo() {
+  
+  // Try to load model again if internet failed
+  if(!classifier){
+    classifier = ml5.imageClassifier(imageModelURL);
+    return;
+  }
+  else if (triedLoadingModel){
+    alert('Listo! Gracias por esperar. Ya puedes asustar a esa persona 😈.');
+    triedLoadingModel = false;
+  }
+  
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  
+  label = results[0].label;
+  confidence = results[0].confidence;
+  setTimeout(classifyVideo, 100);
+  
+}
+
+// To read the image uploaded
+function readImage(file) {
+  
+  // Notify user that the file uploaded is not an image
+  if (file.type && !file.type.startsWith('image/')) {
+    alert('Hmm, parece que esta no es una imagen, intenta con otra.');
+    return;
+  }
+
+  const reader = new FileReader();
+  
+    // Save image to be displayed at the jumpscare
+    reader.addEventListener('load', (event) => {
+    
+    loading = false;
+    selectedImg = loadImage(event.target.result);
+    
+    let ratio = selectedImg.height/selectedImg.width;
+    
+    // Create capture after uploading file, so that mobile users can take pictures of themselves
+    video = createCapture(
+      {video:{
+        facingMode: "user"  
+      }
+    });
+    video.hide();
+      
+    // Start classifying video only when everything is ready
+    
+    setTimeout(function(){
+      flippedVideo = ml5.flipImage(video);
+      classifyVideo();
+    },2000);
+      
+  });
+  
+  reader.readAsDataURL(file);
+  
+}
